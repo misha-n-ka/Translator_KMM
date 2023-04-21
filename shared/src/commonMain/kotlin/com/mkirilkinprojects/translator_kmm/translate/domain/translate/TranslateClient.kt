518968d704e4b25db16dfc23264740356c30b20b
Mikhail Kirilkin <kirilkin_ma@magnit.ru>
2023-04-22 00:37:53 +0700

    create KtorTranslateClient

---
+++ shared/src/commonMain/kotlin/com/mkirilkinprojects/translator_kmm/translate/domain/translate/TranslateClient.kt
@@ -0,0 +1,11 @@
+package com.mkirilkinprojects.translator_kmm.translate.domain.translate
+
+import com.mkirilkinprojects.translator_kmm.core.domain.language.Language
+
+interface TranslateClient {
+    suspend fun translate(
+        fromLanguage:Language,
+        fromText: String,
+        toLanguage: Language
+    ): String
+}
